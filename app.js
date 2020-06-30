f5790801f59c1257e27e0d33474be550df9d03a4
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-06-30 20:37:13 +0200

    tiles hide transition

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector("#hamburger");
 const nav__wraper = document.querySelector("#nav__wraper");
+const tiles = document.querySelector("#tiles");
 const Body = document.querySelector("body");
 hamburger.addEventListener("click", () => {
   nav__wraper.classList.toggle("nav__wraper--show");
+  tiles.classList.toggle("tiles--show");
 });
