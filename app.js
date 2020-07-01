b7066b2405fc16f12ab25a1ccd6026f8a72cb247
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-07-01 11:01:45 +0200

    nav backarrow

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector("#hamburger");
 const nav__wraper = document.querySelector("#nav__wraper");
 const tiles = document.querySelector("#tiles");
 const fog__background = document.querySelector("#fog__background");
+const backarrow = document.querySelector("#backarrow");
 const Body = document.querySelector("body");
 // background to click for closing
 fog__background.addEventListener("click", (e) => {
@@ -10,11 +11,22 @@ fog__background.addEventListener("click", (e) => {
     nav__wraper.classList.toggle("nav__wraper--show");
     tiles.classList.toggle("tiles--show");
     fog__background.classList.toggle("fog__background--show");
+    backarrow.classList.toggle("backarrow--show");
+    hamburger.classList.toggle("hamburger--hide");
   }
+});
+backarrow.addEventListener("click", () => {
+  nav__wraper.classList.toggle("nav__wraper--show");
+  tiles.classList.toggle("tiles--show");
+  fog__background.classList.toggle("fog__background--show");
+  backarrow.classList.toggle("backarrow--show");
+  hamburger.classList.toggle("hamburger--hide");
 });
 // hamburger menu button
 hamburger.addEventListener("click", () => {
   nav__wraper.classList.toggle("nav__wraper--show");
   tiles.classList.toggle("tiles--show");
   fog__background.classList.toggle("fog__background--show");
+  backarrow.classList.toggle("backarrow--show");
+  hamburger.classList.toggle("hamburger--hide");
 });
