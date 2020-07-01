371996138a6bd2509a138d666fb017fc5d20fc93
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-07-01 13:06:42 +0200

    header language change

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const hamburger = document.querySelector("#hamburger");
+const header__wraper = document.querySelector("#header__wraper");
+const languages = document.querySelector("#languages");
+const selected = document.querySelector("#selected");
 const nav__wraper = document.querySelector("#nav__wraper");
 const tiles = document.querySelector("#tiles");
 const fog__background = document.querySelector("#fog__background");
@@ -15,6 +18,7 @@ fog__background.addEventListener("click", (e) => {
     hamburger.classList.toggle("hamburger--hide");
   }
 });
+// back arrow for closing navi
 backarrow.addEventListener("click", () => {
   nav__wraper.classList.toggle("nav__wraper--show");
   tiles.classList.toggle("tiles--show");
@@ -29,4 +33,20 @@ hamburger.addEventListener("click", () => {
   fog__background.classList.toggle("fog__background--show");
   backarrow.classList.toggle("backarrow--show");
   hamburger.classList.toggle("hamburger--hide");
+});
+
+// header click to change category
+header__wraper.addEventListener("click", (e) => {
+  console.log(e.target.tagName);
+  if (e.target.tagName === "P" || e.target.tagName === "DIV") {
+    languages.classList.toggle("languages--show");
+  }
+});
+// change the language
+languages.addEventListener("click", (e) => {
+  if (e.target.tagName === "P") {
+    console.log(e.target.innerText);
+    selected.textContent = e.target.innerText;
+    languages.classList.remove("languages--show");
+  }
 });
