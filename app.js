aa50f3f037f0f18b0e9861e128360dd8fa457981
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-07-08 11:33:50 +0200

    js get input from user

--- app.js
+++ app.js
@@ -10,8 +10,19 @@ const backarrow = document.querySelector("#backarrow");
 const Body = document.querySelector("body");
 const output = document.querySelector("#output");
 const add__form = document.querySelector("#add__form");
+const submit__button = document.querySelector("#submit__button");
 const cancel__button = document.querySelector("#cancel__button");
 import data from "./data.js";
+
+class Snippet {
+  constructor(lang, code, tags, color) {
+    this.lang = lang;
+    this.code = code;
+    this.tags = tags,
+      this.color = color
+  }
+}
+
 // hide all navi elements
 const hideAll = () => {
   language__list.classList.remove("show");
@@ -67,8 +78,8 @@ const loadFiltered = (lang) => {
       container.className = "snippet__container";
       lang === "HTML"
         ? (container.innerHTML = `<div class="container__menu"></div><p class="snippet__text">${htmlConversion(
-            e.code
-          )}</p> <p class="language__tag"></p>`)
+          e.code
+        )}</p> <p class="language__tag"></p>`)
         : (container.innerHTML = `<div class="container__menu"><i class="fas fa-expand fa-2x"></i><i class="far fa-edit fa-2x"></i></div><p class="snippet__text">${e.code}</p> <p class="language__tag"></p>`);
       tags.forEach((e) => {
         container.lastElementChild.textContent += " #" + e;
@@ -117,10 +128,10 @@ nav__list.addEventListener("click", (e) => {
   e.target.classList.contains("navList__element")
     ? e.target.nextElementSibling.firstElementChild.classList.toggle("show")
     : e.target.classList.contains("fas")
-    ? e.target.parentElement.nextElementSibling.firstElementChild.classList.toggle(
+      ? e.target.parentElement.nextElementSibling.firstElementChild.classList.toggle(
         "show"
       )
-    : loadFiltered(e.target.textContent); //get style of clicked element and change heder text & color
+      : loadFiltered(e.target.textContent); //get style of clicked element and change heder text & color
   if (e.target.id === "add__button") {
     add__form.classList.toggle("add__form--show");
     hideAll();
@@ -130,6 +141,15 @@ nav__list.addEventListener("click", (e) => {
   }
 });
 // add snippet
+// submit form to add the snippet
+input__form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let arr = input__form.tags__input.value.split(' ');
+  let tagged = [];
+  tagged = arr.map((e) => "#" + e)
+  console.log(tagged);
+  console.log(input__form.category.value, snippet__input.dataset.color);
+})
 // cancel button to cancel adding snippet
 cancel__button.addEventListener("click", (e) => {
   add__form.classList.remove("add__form--show");
